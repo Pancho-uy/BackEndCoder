b180a6180f381f4929466d050fafb635c2f2df19
Pancho-uy <pancho@cilbeti.om.uy>
2022-11-01 11:24:29 -0300

    firease

--- entrega10/config.js
+++ entrega10/config.js
@@ -9,19 +9,16 @@ const config = {
     },
   },
   firebase: {
-    type: "service_account",
-    project_id: "coder-segunda",
-    private_key_id: "a695143737e91af689851dc0fb4948ee3f89c9ac",
-    private_key:
-      "-----BEGIN PRIVATE KEY-----\nMIIEuwIBADANBgkqhkiG9w0BAQEFAASCBKUwggShAgEAAoIBAQDXY+nWRPZbIdps\nwwjhuacG4UK9rVPORnCF4k6s4Qq0VthDhPJ7XNZsKbHgJcdbhPU5dyj+JHM/Qf6M\n+JyhQjp30a1aen0udqq68kVheY7d0LLbKX0WOmBS7XoNuA/nOmynBG3QZYtwwVKZ\nIN/2YG9RKxrdMCXistmtY/Az5BwnculmM+GRdDLbEqtY/BJgzUHW5yN6RZH5H33A\nzEwu3hm6gsVRG0rGnV+CPQJcxQ5vvoJqe/kABLhy+c0+bv30N2oZ/afqffISt7jg\nycd92DL5Jc63P7A7KbDBB8nI1kZtKsEuyIogI5hYM4SXMZS/chOHDAxZ4to9eh/4\nUYmKBm0FAgMBAAECggEAaai8q/y2cSwhLxtZvwlIk0praAqfwt+xESxb9diLXL4B\nZtXP8Y4wLW0isdc2fYGynhcDR/BvjId+LH9ErqKDdgWoqWrO/7z8sappOJDd8ZgQ\nNQycLQxD9kpChnpVatOxOxkkGYfGqHBdaXrEAuN5l/KyQfD9lMCxoaoVyg7LdNm2\nHR/EGxV7aa4aAuCGE+4duSDUlBKRdk82fLfX2joqNoW+ODntH4LHrvbM2yd6DsbO\nKGf6emCSk34RLUxZOq54ywclu8UpkkzIr/SJVpuh/c2nCS2y8Os1MxUHijOrYduh\nrb/sTxHII1fLK92GP71jLAOWOyBE8IDmYId7WuC2/QKBgQD6zDA7/6jCgVFfWb+V\nDk9SXVmoCnxuhtkmgtsdSuOGCOBdFCpOJsuAQI2ply8CrKXVzL/+iVo+QqQGZ3ls\neew7Y6g+/7SOuhkTgosMtONkuhPxxberBAkdX2XpoHpM9IixAmnLExI0Y4ly+CIF\nFJ4gzHaBBE8jVGz5sQo7OPO6fwKBgQDb27OLvzfuAx9RSuND7gnCWNQfpA+lWLVa\nf9UqQWxvbN3wprAUCCSQxKGgyfJ4S3JUUEkrfQkuQwbh+XNQhh75/FoiPSft24b/\nBlcucf5RCrdlx5nG2EWQAsCVanMqdHXbUgOWFv85sgwMapJV2rhHF8Ke1G4rNe08\nLlb8YBcuewJ/HWPQQzeyjxiYcJQT32PN59kIqKiR7GHhEKJFCym7f/mKUV4Llxz0\nvBLUhwswdx/ET0mvNiTibmlgz19Q22VuSYdkL/3HKvKj/aVOpi6Md0FXFP4Wh/Px\nHYAQcVBdBKejXRrrSuDxQTJo6RAq7G1+GlnbiUL4AyvhmWEl0craxQKBgHj9xSco\nyjy/lyKDleIyQ0ii3cmz3bBeyx7MrqCcdHI1dUYUVV4QxR6uN0xnFnrA+mAP9tRp\n8P12gXkmcCFmTfHAwFGLWkf5wabi9GNo9E3hGeB3Iddd+ZoxCY6wkERS5vEVh0e3\nzCkSJLNIitOYlET7fG0v9pjtCmtODSzCLL+1AoGBAOOGpvimVDus78YWSKmv5S+A\na+ACVC5oVggVrRixFJrvxPOK3LmlAhW4z+uSxmqAc/90E+tHfiQq3mrUDruaU8+8\nNGQ++YSI2sQSk2o08tHhGCweYPlzdr22BSf4bUAUeHm/jL33DNanX62wYJot2ZqN\nALPttWzcQm3II8//b/qb\n-----END PRIVATE KEY-----\n",
-    client_email:
-      "firebase-adminsdk-jeycb@coder-segunda.iam.gserviceaccount.com",
-    client_id: "114765881187855857355",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url:
-      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-jeycb%40coder-segunda.iam.gserviceaccount.com",
+    "type": "service_account",
+    "project_id": "segunda-entrega-367314",
+    "private_key_id": "2fd41b83bca8ea124c68fd8843c936699b1bc91c",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDdWex2UCfMUSPd\nnyPZmMNkvIF+RZTXVfvuVwl5yfWPdTOL6VU+fX+zECan1/jTnXjT+BfqmOEto8mZ\nzVSRIE9Dtp1hzz2CXoVdZYI1vuwQ83yQQp3ytejBBSBflkQTgDRmVnm4pxtDNZmI\n0JdUhz7v7G96IqtpmJT6F5MtyVImtWzYT9r2qiXVZRtGyeRp/KcG0CHIG1tbhyqq\nxo6nskG2numobMaQ2C0uShBZfEsup+EO0f2HuvWyjwlE5Cs4vN9JZwHU2/L6SNoR\n6cAcVXcweQpA22k2vOxLoW0YCORntkENdl6WRkNvyiV6UOVUOXx4wS+jHZonqE0o\nkIp3hR/fAgMBAAECggEASp5+3V+mGvWbfqGgR0F9VR9LFBNG2tTMqgtDUYAovOTI\nrLjLGNbbzrR6I4jmBAiI7AQto9NHzcfyNVwHzyiyL+SG56siVCmjEdofbBjhodFD\nQ6qBbomuXitpP2/7oQtOHtW2EO6cfSq7iGixKziwmdb9uZGocV+TkT+TSRMeE5fj\nBvh0BFofy5/vS7GK7u2ITJ9CzTVVQ5jt0V+7Oo7u4YwRUiRLUUoZFyEUNoqEi8dL\nLI+5vUaGnoT7kifgpG3RD2xyoEF9dija+W6yqjFAowfWcW+dwBWDZP3z7uOSQiRx\n2WWzIvjcBQ/A7dq/7Zcwhcl2zSCk31hul8x0BNNM6QKBgQDxkQZAX/o4PPI4W3ie\nPWklwFBr+AUsGMcCGtagMD+MUSYREzakqfNhKWeGKb4f5S0WMZwl/nkXSzUUWtgk\ngISiHPGepVcYbqP7kuNKvHWfBsFx0ud+0UJ6/ke4AvOejRU9/kLStHM17LDblPBS\nWXhVlwtPwXC8te1zaxp/ZK0x8wKBgQDqk7BrOL8m97BG+wUvIgmxkd5IEDWsc5zE\nCcikr7brHCLYlJcXHnBW4S6nQIZFQzN+7wVzFcwtTTn5d2temzkFb4JCrMP/IYLz\niRk0q/4rqIkZ8zf0XXH3d3F/QsQrzB0IIvCcjG/jWLb7PtLuuXVPqG8mYkdHSJRe\nnCKguK2pZQKBgAhBrLUGksbZTm4eskC2WKGxsQ3/rC2VqzenMy7Gd2WUYHi072kx\nZeAsSh2Lz7PHYYMgrvWdfQYOzAGmGT0mJ2JjGt8RR6jOH0y5+IrVZi/JS1NYohDz\nlhIeHtVrRGhm6hL0XdRV1ppBPMpEjPJDnJlQUFKpifDpRvRq9Lzl9AWBAoGBAOUz\nZ0ZszQFjNJ82OCuEg5o4H8uj4v3cfQljUePwOLdi531dOoABYtzQ64fKWSel8NQO\n8fEwFC8TbRkeoQ9wdXelK87ABkXsVYplo/Y+M3wvEb8EUeD3yRp2X7Rr5UeyCY/g\nq98arVyjWy8EhI9zMHej6O7FSkYRWb9TqdYGfNCtAoGAYrGzqEuvOLZrxRRWEm/s\nmlylotN3Glei/pSjmKfWtE24+v07gPzvsRy4FdL/kd6glkxbLpOo+Vlyyfz380or\neFgY5ay/+hWm63sHbeBLMYjL6vZ3gUXfQ23ycWqnJG0FvPGK5A6fGu207hkbQhkY\nG2AFRqNgpNR+w/LBiOVGOhg=\n-----END PRIVATE KEY-----\n",
+    "client_email": "pancho@segunda-entrega-367314.iam.gserviceaccount.com",
+    "client_id": "113747973439665176816",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/pancho%40segunda-entrega-367314.iam.gserviceaccount.com"
   },
 };
 
