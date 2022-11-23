1c98bbea96acb5e8298b76351c596164927ee3b2
Pancho-uy <pancho@cilbeti.om.uy>
2022-11-23 09:59:43 -0300

    13

--- entrega13/usuarios/users.js
+++ entrega13/usuarios/users.js
@@ -1,3 +1,6 @@
+//
+// ATENCION, LA IMPORTACION DE ESTE MODULO NO ME FUNCIONA EN EL SERVIDOR, ASI QUE COPIE ESTO EN EL ARCHIVO SERVER.JS
+
 const users = [ { id: 1, username: 'Pancho-uy', password: 'admin', admin:true } ]
 
 module.exports = { users }
