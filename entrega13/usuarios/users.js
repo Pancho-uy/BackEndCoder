e82a682e143aa83f970a3269347ae8a113f4c5fd
Pancho-uy <pancho@cilbeti.om.uy>
2022-11-23 10:05:23 -0300

    COMENTARIOS EN CODIGO

--- entrega13/usuarios/users.js
+++ entrega13/usuarios/users.js
@@ -1,6 +1,6 @@
-//
-// ATENCION, LA IMPORTACION DE ESTE MODULO NO ME FUNCIONA EN EL SERVIDOR, ASI QUE COPIE ESTO EN EL ARCHIVO SERVER.JS
+// ATENCION, LA IMPORTACION DE ESTE MODULO NO ME FUNCIONA EN EL SERVIDOR, ASI QUE COMENTE ESTO Y
+// COPIE ESTE EN EL ARCHIVO SERVER.JS
 
-const users = [ { id: 1, username: 'Pancho-uy', password: 'admin', admin:true } ]
+/* const users = [ { id: 1, username: 'Pancho-uy', password: 'admin', admin:true } ]
 
-module.exports = { users }
+module.exports = { users } */
